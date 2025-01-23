98a4ae9700a0f0c59cf9ce1b6a5d9a30237714e3
Tiisetso <hlabirwa.me@gmail.com>
2025-01-24 00:51:18 +0200

    chore: fix module exports

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,4 +1,8 @@
-export const DOMAIN =
+const DOMAIN =
   process.env.NODE_ENV === "production"
     ? "https://elcassica.invoicegen.co.za"
     : "http://localhost:3000";
+
+module.exports = {
+  DOMAIN,
+};
